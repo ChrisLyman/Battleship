ec660524e176658ac32e4ce33090ee38a5a17bce
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-13 10:22:00 -0400

    add empty? and  place_ship methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,22 @@
 class Cell
 
-  attr_reader :coordinate
-  
+  attr_reader :coordinate,
+              :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end 
+  end
+
+  def place_ship(boat)
+    @ship = boat
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe Cell do
       expect(@cell.coordinate).to eq("B4")
     end
   end
+
+  describe '@ship' do
+    it 'has a default value of nil' do
+      expect(@cell.ship).to be nil
+    end
+  end
+
+  describe '#empty' do
+    it 'returns true if cell does not contain a ship' do
+      expect(@cell.empty?).to be true
+    end
+  end
+
+  describe '#place_ship' do
+    it 'can place a ship' do
+      @cell.place_ship(@cruiser)
+
+      expect(@cell.ship).to eq(@cruiser)
+      expect(@cell.empty?).to be false
+    end
+  end
 end
