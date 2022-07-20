4588603ff76e57d17f776dfcd05f90cd5881d662
Ashley <ashuhleyt@gmail.com>
2022-07-19 18:51:23 -0700

    refactor game.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,19 +10,24 @@ RSpec.describe Game do
 
     describe 'initialize' do
       it 'exists' do
-
-        expect(game).to be_instance_of(Game)
+        expect(@game).to be_instance_of(Game)
       end
     end
 
     describe 'start' do
       it 'puts out two start messages' do
-
         expect(@game.start).to eq("Welcome to BATTLESHIP")
       end
     end
+
+      it 'has the ships on the board' do
+         ship = Ship.new("Cruiser", 3)
+         allow(@game).to receive(:generate_random_coordinates).and_return(["B2", "B3", "B4"], ["A1", "B1"])
+         @game.place_cpu_cruiser
+         @game.place_cpu_submarine
+
+         expect(@game.cpu_board.render(true)).to eq("  1 2 3 4 \nA S . . . \nB S S S S \nC . . . . \nD . . . . \n")
+       end
+
+       
 end
-#another thought: creating separate classes for player and computer?
-#seems like it might be easier to separate them out, vs forcing them to work so hard together?
-#lmk your thoughts, we have plenty of project work time during the week.
-#see ya!
